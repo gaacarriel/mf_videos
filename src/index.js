8fc67bf9ac46834fa5495a3ee81d6b376da2e8e2
Gabriel Carriel <ga123carriel2015@gmail.com>
2024-07-03 00:51:15 -0300

    adding function to favorite the video and save to localstorage

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+function drawerFunc(video) {
+    const drawerList = JSON.parse(localStorage.getItem("@videostubeDrawerList"))
+
+    if(drawerList){
+        const videoIndex = drawerList.findIndex(videoDrawerList => video.id.videoId === videoDrawerList.id.videoId);
+
+        if (videoIndex > -1) {
+            drawerList.splice(videoIndex, 1);
+        } else {
+            drawerList.push(video);
+        }
+
+        localStorage.setItem("@videostubeDrawerList", JSON.stringify(drawerList));
+    }else{
+        newList = []
+        newList.push(video)
+        localStorage.setItem("@videostubeDrawerList", JSON.stringify(newList))
+    }
+}
+
 async function searchVideos(filter) {
     const url = `http://localhost:3002/api/search?q=${filter || ""}`
     try {
@@ -16,13 +36,17 @@ async function searchVideos(filter) {
             const card = document.createElement("li")
             const thumbnail = document.createElement("img")
             const title = document.createElement("p")
+            const drawerBtt = document.createElement("input")
             
             thumbnail.src = item.snippet.thumbnails.high.url
             title.textContent = item.snippet.title
             title.className = "limited-text"
+            drawerBtt.type = "checkbox"
+            drawerBtt.onclick = () => drawerFunc(item);
             
             card.append(thumbnail)
-            card.append(title)   
+            card.append(title)
+            card.append(drawerBtt)   
             div.append(card)
         })
     } catch (error) {
